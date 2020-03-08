4b13050ece6bcb46f831cae3084839cd00e20f99
Romain Elineau <romain.elineau@gmail.com>
2020-03-08 16:09:34 +0100

    Fichiers de traduction en français

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé un email avec le lien de réinitialisation du mot de passe !',
+    'throttled' => 'Veuillez patienter quelques minutes avant de relancer.',
+    'token' => 'Le token de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne parvenons pas à trouver d'utlisateur ayant cette adresse email.",
+
+];
